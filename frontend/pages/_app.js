6d2e76c186271b9873c822ca9f99ad01d59c9f41
srikarsam <srikar_samudrala@condenast.com>
2021-02-15 20:22:50 +0530

    styled components setup

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,12 @@
-import PropTypes from 'prop-types';
-import Page from '../components/Page';
+import PropTypes from "prop-types";
+import Page from "../components/Page";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.start());
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -12,4 +19,4 @@ export default function MyApp({ Component, pageProps }) {
 MyApp.propTypes = {
   Component: PropTypes.any,
   pageProps: PropTypes.object,
-}
+};
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,13 +1,21 @@
 import Document, { Html, Head, NextScript, Main } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class myDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en-IN">
-        <Head>
-          <title>Slick Fits</title>
-        </Head>
         <body>
+          <Head />
           <Main />
           <NextScript />
         </body>
